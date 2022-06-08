31153718d84de3192b80451e131d11735e083775
wadebooth <wademgmt@gmail.com>
2022-06-08 14:13:23 -0400

    setup initial communication between react and express

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running...')
 })
 
 app.get('/api/products', (req, res) => {
-    res.json(products)
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(products)
 })
 
 app.listen(5050, () => {
- console.log('Server running on port 5050')})
-
-
-
-
- 
+  console.log('Server running on port 5050')
+})
