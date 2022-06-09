21859d6503a641a30c6957945b5f7dd51af390d7
wadebooth <wademgmt@gmail.com>
2022-06-09 17:31:09 -0400

    added product model to schema

--- models/productModel.js
+++ models/productModel.js
@@ -1,21 +1,71 @@
 import mongoose from 'mongoose'
 
-const productSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: String, required: true },
+    comment: { type: String, required: true },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+  {
+    timestamps: true,
+  }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // which user (admin only) created the product
+      required: true,
+      ref: 'User', //adds a relationship between the product and the user
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema], //see above schema called reviewSchema
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
-  password: {
-    tyype: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default User
